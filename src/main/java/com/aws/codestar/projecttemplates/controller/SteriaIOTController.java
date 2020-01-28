e314e5e050e28555eb69c99290a610d75048f16c
GUNGORDU Aydin <aydinbg@gmail.com>
2020-01-28 12:03:29 +0100

    Fix bug

--- src/main/java/com/aws/codestar/projecttemplates/controller/SteriaIOTController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/SteriaIOTController.java
@@ -7,8 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Map;
+import java.util.Date;
 
 /**
  * Basic Spring web service controller that handles all GET requests.
@@ -17,6 +23,7 @@ import java.util.Map;
 @RequestMapping("/")
 public class SteriaIOTController {
 
+    private static Date lastUpdate = Calendar.getInstance().getTime();
     private static boolean isTouched = false;
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
@@ -25,9 +32,19 @@ public class SteriaIOTController {
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity setIsTouched(@RequestBody Map<String, Object> payload) {
+    public ResponseEntity setIsTouched(@RequestBody Map<String, Object> payload) throws ParseException {
         System.out.println(payload.get("timestamp"));
-        isTouched = true;
+        String timestamp = "" + payload.get("timestamp");
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        Date date = format.parse(timestamp);
+
+        if (date.after(lastUpdate)) {
+            isTouched = true;
+            lastUpdate = date;
+            System.out.println("IS TOUCHED AND " + timestamp + " is after " + lastUpdate.toString());
+        }
+
         return ResponseEntity.ok(createResponse("true"));
     }
 
